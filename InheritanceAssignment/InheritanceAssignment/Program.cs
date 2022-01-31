e45feb20fc9c202780561179c5ccaa7911ea14ff
Mayank Kishor <kishormayank206@gmail.com>
2022-01-31 19:07:27 +0530

    Inheritance Assignment 4

---
+++ InheritanceAssignment/InheritanceAssignment/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAssignment
+{
+
+    class Furniture
+    {
+        public int orderId;
+        public DateTime orderDate;
+        public string furnitureType;
+        public int qty;
+        public double amount;
+        public string paymentMode;
+
+        public virtual void GetData()
+        {
+            Console.WriteLine("Enter orderID, Order date, furniture type, quantity, payment mode: ");
+            orderId=int.Parse(Console.ReadLine());
+            orderDate=DateTime.Parse(Console.ReadLine());
+            furnitureType=Console.ReadLine();
+            qty=int.Parse(Console.ReadLine());
+            paymentMode=Console.ReadLine();
+
+        }
+
+        public virtual void ShowData()
+        {
+            Console.WriteLine("Order ID, date, furniture type, quantity, payment mode: ");
+            Console.WriteLine(orderId);
+            Console.WriteLine(orderDate);
+            Console.WriteLine(qty);
+            Console.WriteLine(paymentMode);
+
+        }
+
+    }
+
+    class Chair: Furniture
+    {
+        public string chairType;
+        public string Chairpurpose;
+        public double rate;
+
+        public override void GetData()
+        {
+            Console.WriteLine("enter chairtype(wood, steel, plastic), chairpurpose, rate:");
+            chairType=Console.ReadLine();
+            Chairpurpose=Console.ReadLine();
+            rate=double.Parse(Console.ReadLine());
+        }
+
+        public override void ShowData()
+        {
+            
+            amount = qty * rate;
+            Console.WriteLine("amount: " + amount);
+        }
+
+    }
+
+    class cot : Furniture
+    {
+        public string cotType;
+        public string capacity;
+        public double rate;
+
+        public override void GetData()
+        {
+
+            Console.WriteLine("enter chairtype, chairpurpose, rate:");
+            cotType = Console.ReadLine();
+           capacity = Console.ReadLine();
+            rate = double.Parse(Console.ReadLine());
+        }
+
+        public override void ShowData()
+        {
+            amount = qty * rate;
+            Console.WriteLine("amount: " + amount);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Furniture obj = null;
+            obj = new Furniture();
+            obj.GetData();
+            if(obj.furnitureType=="chair")
+            {
+                Chair obj1 = new Chair();
+                obj1.GetData();
+                obj1.ShowData();
+            }
+            else
+            {
+              cot obj2 = new cot();
+                obj2.GetData();
+                obj2.ShowData();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InheritanceAssignment/InheritanceAssignment/ex2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAssignment
+{
+    class Building
+    {
+        public string type;
+        public string capacity;
+        public string dimension;
+        public int floorNo;
+        public DateTime dateOfCompletion;
+
+        public Building()
+        {
+            Console.WriteLine("Enter type, capacity & date of completion: ");
+            type=Console.ReadLine();
+            capacity=Console.ReadLine();
+            dateOfCompletion=DateTime.Parse(Console.ReadLine());
+        }
+
+        public void showdata()
+        {
+            Console.WriteLine("Type:  Capacity:  Date of completion: ",type+" "+ capacity+ " "+dateOfCompletion);
+            if (type == "flat")
+            {
+                Console.WriteLine("Enter floor: ");
+                floorNo=int.Parse(Console.ReadLine());
+                Console.WriteLine("Flat is available at: ",+floorNo);
+            }
+            if (type == "villa")
+            {
+                Console.WriteLine("Enter dimension");
+                dimension=Console.ReadLine();
+                Console.WriteLine("dimensions are: " + dimension);
+            }
+        }
+    }
+    internal class ex2
+    {
+        static void Main(string[] args)
+        {
+            Building building = new Building();
+            building.showdata();
+            Console.ReadKey();
+
+
+        }
+    }
+}
